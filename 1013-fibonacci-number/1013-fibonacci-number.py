f68b104a75465a5db715c7dd9a561db0dbac5fea
Praveen Kumar Bandaru <bandarupraveen48@gmail.com>
2025-05-12 08:25:58 +0530

    Time: 598 ms (32.82%), Space: 12.3 MB (98.35%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def fib(self, n):
-        ans=[0,1]
-        for i in range(2,n+1):
-            ans.append(ans[i-1]+ans[i-2])
-        return ans[n]
+        # ans=[0,1]
+        # for i in range(2,n+1):
+        #     ans.append(ans[i-1]+ans[i-2])
+        # return ans[n]
+        if n<=1:
+            return n
+        return self.fib(n-1)+self.fib(n-2)
