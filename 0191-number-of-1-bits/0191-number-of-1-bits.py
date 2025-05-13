f70ada23eb6539229091ba8c70772eea7630e36e
Praveen Kumar Bandaru <bandarupraveen48@gmail.com>
2025-05-13 10:22:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.21%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def hammingWeight(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        """ using Brian Kernighan's Algorithm"""
         count = 0;
         while(n != 0):
             n = n & (n-1)
